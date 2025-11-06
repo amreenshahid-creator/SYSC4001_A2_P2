194faf0e2df1e10395f205bc80b8b49f5412744a
Samy Kaddour <141351381+SamyKadd@users.noreply.github.com>
2025-11-06 17:35:09 -0500

    Completed Part 2

--- process2.c
+++ process2.c
@@ -1,1 +1,56 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
 
+#define SHM_KEY 0x1234
+struct SharedData{
+    int multiple;
+    int counter;
+};
+
+int main(void){
+    int shmid = shmget(SHM_KEY, sizeof(struct SharedData),0666);
+    if (shmid == -1){
+        perror("process2: shmget failed");
+        return 1;
+    }
+
+    struct SharedData *data = (struct SharedData*) shmat(shmid,NULL,0);
+    if (data == (void*)-1){
+        perror("process2: shmat failed");
+        return 1;
+    }
+
+    while ((data->counter) <=100){
+        sleep(1);
+    }
+
+    int counter2 = 0;
+    int cycle = 0;
+    pid_t pid = getpid();
+
+    while ((data->counter) <=500){
+        int m = data->multiple;
+        int isMult = ((m != 0) && (counter2 % m ==0)); //checking divisbility
+        if (isMult){
+            printf("Cycle number: %d - %d is a multiple of %d (child %d) [shared.counter=%d]\n",cycle,counter2,m,pid,data->counter);
+
+        }else {
+            printf("Cycle number: %d (child %d) [shared.counter=%d]\n",cycle,pid,data->counter);
+
+        }
+
+        counter2--;
+        sleep(1);
+        cycle++;
+    }
+
+    if (shmdt(data) == -1){
+        perror("process2: shmdt failed");
+    }
+
+    return 0;
+}
